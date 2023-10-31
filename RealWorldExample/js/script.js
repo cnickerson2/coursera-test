a97f60ff06eba693be946a2e8e7d0325040c169b
Chris Nickerson <cnickerson2@me.com>
2023-10-31 08:33:41 -0600

    Added home-snippet loading functionality

--- RealWorldExample/js/script.js
+++ RealWorldExample/js/script.js
@@ -7,3 +7,34 @@ $(function (){
 	});
 });
 
+(function (global){
+	var dc = {};
+
+	var homeHtml = "snippets/home-snippet.html";
+
+	var insertHtml = function (selector, html) {
+		var targetElem = document.querySelector(selector);
+		targetElem.innerHTML = html;
+	};
+
+	var showLoading = function (selector) {
+		var html = "<div class='text-center'>";
+		html += "<img src='images/ajax-loader.gif'></div>";
+		insertHtml(selector, html);
+	};
+
+	document.addEventListener(
+		"DOMContentLoaded", 
+		function(event){
+			showLoading("#main-content");
+			$ajaxUtils.sendGetRequest(
+				homeHtml, 
+				function(responseText){
+					document.querySelector("#main-content").innerHTML = responseText;
+				}, 
+				false);
+		}
+	);
+
+	global.$dc = dc;
+})(window);
